8f0559d38d7dd92e83cca4dd4ab741792dcbdce2
Kamila Zacariotto <kamilazacariotto@gmail.com>
2019-11-22 18:01:41 -0300

    Scripts de insert no banco

---
+++ backend/SQLScripts/10-INSERT_INTO_TABLE_PlanType.sql
@@ -0,0 +1,9 @@
+INSERT INTO PlanType(Name,Active) VALUES ('Semanal',1);
+INSERT INTO PlanType(Name,Active) VALUES ('Mensal',1);
+INSERT INTO PlanType(Name,Active) VALUES ('Bimestral',1);
+INSERT INTO PlanType(Name,Active) VALUES ('Trimestral',1);
+INSERT INTO PlanType(Name,Active) VALUES ('Semestral',1);
+INSERT INTO PlanType(Name,Active) VALUES ('Anual',1);
+
+
+
---
+++ backend/SQLScripts/9-INSERT_INTO_TABLE_Service.sql
@@ -0,0 +1,21 @@
+INSERT INTO Service (Name, SerivcePicture, Active) VALUES ('Twitch', '',1);
+INSERT INTO Service (Name, SerivcePicture, Active) VALUES ('Spotify', '',1);
+INSERT INTO Service (Name, SerivcePicture, Active) VALUES ('Netflix', '',1);
+INSERT INTO Service (Name, SerivcePicture, Active) VALUES ('Prime Video ', '',1);
+INSERT INTO Service (Name, SerivcePicture, Active) VALUES ('Amazon Prime', '',1);
+INSERT INTO Service (Name, SerivcePicture, Active) VALUES ('Crunchyroll', '',1);
+INSERT INTO Service (Name, SerivcePicture, Active) VALUES ('Deezer', '',1);
+INSERT INTO Service (Name, SerivcePicture, Active) VALUES ('Apple Music', '',1);
+INSERT INTO Service (Name, SerivcePicture, Active) VALUES ('Youtube', '',1);
+INSERT INTO Service (Name, SerivcePicture, Active) VALUES ('Kindle Unlimited', '',1);
+INSERT INTO Service (Name, SerivcePicture, Active) VALUES ('Audible', '',1);
+
+
+
+
+
+
+ 
+
+
+
