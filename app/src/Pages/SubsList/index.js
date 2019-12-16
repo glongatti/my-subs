82e7b23757c4d8d49789ba8b2956ba3331819668
Gabriel <longattigabriel@gmail.com>
2019-12-16 20:37:53 -0300

    - add componente page, subs list

---
+++ app/src/Pages/SubsList/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import { PageTitleText } from './styles';
+// import { Container } from './styles';
+
+export default function SubsList() {
+  return (
+    <View>
+      <PageTitleText>Minhas assinaturas</PageTitleText>
+    </View>
+  );
+}
---
+++ app/src/Pages/SubsList/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+import colors from '../../utils/colors';
+
+export const PageTitleText = styled.Text`
+  color:${colors.primaryGreen};
+  fontSize:28;
+  marginTop:10;
+  marginLeft:15;
+  borderBottomWidth:1;
+  borderBottomColor:${colors.primaryGreen};
+  paddingBottom:10
+`;
